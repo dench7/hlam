2ace6b2f52b19132191e243110af02a2d0c95406
dench7 <89294662+dench7@users.noreply.github.com>
2022-04-04 21:02:01 +0300

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -87,16 +87,16 @@ After=network.target
 [Service]
 Type=simple
 User=$USER
-ExecStart=$(which subspace) \\
---chain testnet \\
---wasm-execution compiled \\
---execution wasm \\
---bootnodes "/dns/farm-rpc.subspace.network/tcp/30333/p2p/12D3KooWPjMZuSYj35ehced2MTJFf95upwpHKgKUrFRfHwohzJXr" \\
---rpc-cors all \\
---rpc-methods unsafe \\
---ws-external \\
---validator \\
---telemetry-url "wss://telemetry.polkadot.io/submit/ 1" \\
+ExecStart=$(which subspace) 
+--chain testnet \
+--wasm-execution compiled \
+--execution wasm 
+--bootnodes "/dns/farm-rpc.subspace.network/tcp/30333/p2p/12D3KooWPjMZuSYj35ehced2MTJFf95upwpHKgKUrFRfHwohzJXr" \
+--rpc-cors all \
+--rpc-methods unsafe \
+--ws-external \
+--validator \
+--telemetry-url "wss://telemetry.polkadot.io/submit/ 1" \
 --name $NICKNAME
 Restart=on-failure
 RestartSec=10
