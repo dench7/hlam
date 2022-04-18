63d96130c83f937c55b4b3cdb9ed52b7a2fd77fd
dench7 <89294662+dench7@users.noreply.github.com>
2022-04-18 21:07:56 +0300

    Update loka.sh

--- loka.sh
+++ loka.sh
@@ -22,42 +22,26 @@ echo "============================================================"
 
 sudo apt-get update
 sudo apt-get install jq -y
-sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
-sudo chmod a+x /usr/local/bin/yq
-sudo apt install python3-pip
-sudo apt install pipenv
-echo "=================================================="
 
-echo -e "\e[1m\e[32m2. Checking if Docker is installed... \e[0m" && sleep 1
+sleep 3
 
-if ! command -v docker &> /dev/null
-then
+echo "======Install Python======"
+sudo apt install python3-pip -y
+sudo apt install pipenv -y
 
-    echo -e "\e[1m\e[32m2.1 Installing Docker... \e[0m" && sleep 1
-    sudo apt-get install ca-certificates curl gnupg lsb-release wget -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-fi
+sleep 3
 
-echo "=================================================="
+echo "======Install Docker======"
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/installers/docker.sh)
 
-echo -e "\e[1m\e[32m3. Checking if Docker Compose is installed ... \e[0m" && sleep 1
+sleep 3
 
-docker compose version &> /dev/null
-if [ $? -ne 0 ]
-then
-
-    echo -e "\e[1m\e[32m3.1 Installing Docker Compose v2.3.3 ... \e[0m" && sleep 1
-    mkdir -p ~/.docker/cli-plugins/
-    curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-    chmod +x ~/.docker/cli-plugins/docker-compose
-    sudo chown $USER /var/run/docker.sock
-fi
-
-echo "=================================================="
+echo "=====Install Bot====="
 
 git clone https://github.com/hldh214/lok_bot.git
-
 cd lok_bot
+docker buildx build -t lok_bot_local --build-arg PYPI_MIRROR=https://pypi.tuna.tsinghua.edu.cn/simple .
+
+echo "Бот готов ,укажи айди профиля в команде docker run -e TOKEN=YOUR_X_ACCESS_TOKEN lok_bot_local"
+
+done
