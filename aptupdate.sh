154e1e1bb9d310dbda21fa920dfc11c8af90449f
dench7 <89294662+dench7@users.noreply.github.com>
2022-06-16 15:11:58 +0300

    Create aptupdate.sh

---
+++ aptupdate.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Logo
+echo "==============================================================="
+echo -e "\033[0;35m"
+echo "                                                               ";
+echo "  11      11    111111     111111   11111111      1111111      ";
+echo "  111     11  11      11 11      11 11      11  11       11    ";
+echo "  1111    11  11      11 11      11 11      11  11       11    ";
+echo "  11 11   11  11      11 11      11 11      11  11             ";
+echo "  11  11  11  11      11 11      11 11111111      1111111      ";
+echo "  11   11 11  11      11 11      11 11      11           11    ";
+echo "  11    1111  11      11 11      11 11      11  11       11    ";
+echo "  11     111  11      11 11      11 11      11  11       11    ";
+echo "  11      11    111111     111111   11111111      1111111      ";
+echo "                                                               ";
+echo -e "\e[0m"
+echo "================================================================"
+
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+echo "=============Updates============="
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+sleep 3
+
+cd testnet
+docker compose down
+
+rm -rf ~/docker-compose.yaml
+rm -rf ~/public_full_node.yaml
+rm -rf ~/genesis.blob
+rm -rf ~/waypoint.txt
+
+sleep 3
+
+wget https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/public_full_node/docker-compose.yaml
+wget https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/public_full_node/public_full_node.yaml
+wget https://devnet.aptoslabs.com/genesis.blob
+wget https://devnet.aptoslabs.com/waypoint.txt
+
+sleep 2
+
+docker-compose up
