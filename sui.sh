fcd31a15fa9b1906de56544b8ec0447f294bdff1
dench7 <89294662+dench7@users.noreply.github.com>
2022-08-02 22:04:36 +0300

    Create sui.sh

---
+++ sui.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+systemctl stop suid 
+
+rm -rf /var/sui/db/* /var/sui/genesis.blob $HOME/sui
+
+source $HOME/.cargo/env
+
+cd $HOME
+
+git clone https://github.com/MystenLabs/sui.git
+
+cd sui
+
+git remote add upstream https://github.com/MystenLabs/sui
+
+git fetch upstream
+
+git checkout -B devnet --track upstream/devnet
+
+cargo build --release
+
+mv ~/sui/target/release/sui-node /usr/local/bin/
+
+mv ~/sui/target/release/sui /usr/local/bin/
+
+wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+
+systemctl restart suid
