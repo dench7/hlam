782ed4e8fb912df64585d0d8fe8e434f0956b51a
dench7 <89294662+dench7@users.noreply.github.com>
2022-08-23 11:18:27 +0300

    Update and rename sub.sh to stark.sh

---
+++ stark.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~/pathfinder
+
+git fetch
+
+git checkout v0.3.1
+
+cargo build --release --bin pathfinder
+
+mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
+
+cd py
+
+source .venv/bin/activate
+
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
+
+systemctl restart starknetd
